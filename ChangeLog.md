c7ff4115c0c1bf57d67fd6127e4bb806786387e0
ronin4net <pinguin751@gmx.net>
2017-01-06 14:51:56 +0100

    carve-out from Plainion.Core repo

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+## 1.3.1.0 - 2016-08-13   
+
+- Upgraded to Plainion.Core 3.0
---
+++ src/Plainion.AppFw.Shell/Properties/AssemblyDefs.cs
@@ -0,0 +1,4 @@
+using System.Windows.Markup;
+[assembly: XmlnsPrefix( "http://github.com/ronin4net/plainion/appfw/shell", "pnfw" )]
+[assembly: XmlnsDefinition( "http://github.com/ronin4net/plainion/appfw/shell", "Plainion.AppFw.Shell" )]
+
